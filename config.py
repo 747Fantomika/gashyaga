e9a804d6b4e4da001b3e707767945f3ba1bd4ce6
747Fantomika <minaevvadim40@gmail.com>
2026-09-13 01:19:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-ADMIN_ID = int(os.getenv("ADMIN_ID", "5340089282"))
-BOT_TOKEN = "8994806358:AAHBrc3jd7Qzz3ntcBK5XqZrUA5odw2oHHY"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "ЗАГЛУШКА")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
