63c93b4b7235e797398e8ba805ae36a259e825ed
747Fantomika <minaevvadim40@gmail.com>
2026-09-13 01:32:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from aiogram.types import (
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import Command
 
 from config import BOT_TOKEN, ADMIN_ID
 
